66bc57affabcfc72aca27743ec333968ea42eecc
Kyryl2 <krasiuk.4884@gmail.com>
2024-05-06 16:22:09 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,6 @@
-import PetFormFormik from './Forms/Form-Formik';
-import PetFormRHF from './Forms/Form-RHF';
-import PetForm from './Forms/Form-Usuall';
 import s from './App.module.css';
 const App = () => {
-  return (
-    <div className={s.container}>
-      <h1 className={s.header}> Usuall form</h1>
-      <PetForm />
-      <h1 className={s.header}> Formik form</h1>
-      <PetFormFormik />
-      <h1 className={s.header}> React-Hook-Form form</h1>
-      <PetFormRHF />
-    </div>
-  );
+  return <div className={s.container}></div>;
 };
 
 export default App;
